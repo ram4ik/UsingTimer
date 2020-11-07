f6f2538f55357607249b4563da0f177a0e8dbdab
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-07 09:48:09 +0200

    Initial Commit

---
+++ UsingTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingTimer
+//
+//  Created by Ramill Ibragimov on 07.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
