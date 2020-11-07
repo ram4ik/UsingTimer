c7590b3341641fc7ad7cd4c047ceeb78b67d0623
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-07 10:10:21 +0200

    timer tolerance

--- UsingTimer/ViewController.swift
+++ UsingTimer/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let colors: [UIColor] = [
+        .systemRed,
+        .systemBlue,
+        .systemPink,
+        .systemTeal,
+        .systemGray,
+        .systemFill,
+        .systemGreen,
+        .systemOrange,
+        .systemYellow,
+        .systemPurple
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        createTimer()
     }
 
-
+    func createTimer() {
+        let timer = Timer.scheduledTimer(
+            timeInterval: 2,
+            target: self,
+            selector: #selector(fireTimer),
+            userInfo: nil,
+            repeats: true
+        )
+        timer.tolerance = 200
+    }
+    
+    @objc func fireTimer() {
+        view.backgroundColor = colors.randomElement()
+    }
 }
 
